363f3b69f26f0c12e35e3d31357a29bfebb1a8fc
Jahmora <Miphonej13@gmail.com>
2024-10-28 23:34:47 +0100

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,4 +1,6 @@
-// translation.js
+// translations.js
+
+// Objet contenant les traductions pour l'interface utilisateur du quiz, par langue
 const translations = {
     fr: {
         welcome: "Bienvenue au Quiz de Jeux Vidéo",
@@ -10,7 +12,10 @@ const translations = {
         endGame: "Fin du Jeu!",
         playAgain: "Rejouer",
         backToMenu: "Retour au menu principal",
-        loadingNextQuestion: "Chargement de la prochaine question..."
+        loadingNextQuestion: "Chargement de la prochaine question...",
+        levelUp: "Félicitations! Vous pouvez passer au niveau supérieur!",
+        // Message ajouté pour notifier qu'une nouvelle question a été ajoutée
+        newQuestionAdded: "Une nouvelle question a été ajoutée au quiz!"
     },
     en: {
         welcome: "Welcome to the Video Game Quiz",
@@ -22,6 +27,10 @@ const translations = {
         endGame: "Game Over!",
         playAgain: "Play Again",
         backToMenu: "Back to Main Menu",
-        loadingNextQuestion: "Loading the next question..."
+        loadingNextQuestion: "Loading the next question...",
+        levelUp: "Congratulations! You can move to the next level!",
+        // Message ajouté pour notifier qu'une nouvelle question a été ajoutée
+        newQuestionAdded: "A new question has been added to the quiz!"
     }
 };
+
