15a48d110a952de9e02af7011130a9ec0fe618a8
Jahmora <Miphonej13@gmail.com>
2024-10-28 13:50:14 +0100

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,28 @@
+// translations.js
+
+const translations = {
+    fr: {
+        levelSelect: "Choisissez un niveau:",
+        invalidLevel: "Niveau invalide. Veuillez sélectionner un niveau valide.",
+        correct: "Correct !",
+        incorrect: "Incorrect !",
+        loadingMessage: "Chargement de la prochaine question...",
+        finalScore: "Votre score :",
+        playAgain: "Rejouer",
+        mainMenu: "Retour au menu principal",
+        introTitle: "Studio Jahmora Production 🎮",
+        welcomeMessage: "Bienvenue au Quiz de Jeux Vidéo"
+    },
+    en: {
+        levelSelect: "Select a level:",
+        invalidLevel: "Invalid level. Please select a valid level.",
+        correct: "Correct!",
+        incorrect: "Incorrect!",
+        loadingMessage: "Loading the next question...",
+        finalScore: "Your score:",
+        playAgain: "Play Again",
+        mainMenu: "Back to Main Menu",
+        introTitle: "Studio Jahmora Production 🎮",
+        welcomeMessage: "Welcome to the Video Game Quiz"
+    }
+};
